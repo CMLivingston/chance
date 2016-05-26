7f94ace2cc3bd53cd46d7927dda6c15610951059
Chris Livingston <clivingston45@gmail.com>
2016-05-26 02:35:56 -0400

    follower relationships setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,30 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :matches
+  has_many :matches, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :active_relationships, source: :follower
   
 
   # here is the current email validation
   validates :email, format: { with: /\b[A-Z0-9._%a-z\-]+@dartmouth.edu\z/, message: ": Sorry! You must have a Dartmouth email account to sign up." }
+
+
+
+  # helper methods
+
+  def follow(other)
+  	active_relationships.create(followed_id: other.id)
+  end
+
+  def unfollow(other)
+  	active_relationships.find_by(followed_id: other.id).destroy
+  end
+
+  def following?(other)
+  	following.include?(other)
+  end
+
 end
