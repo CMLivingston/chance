80d808e349834612c4fde5c890c6167ecb17095f
Chris Livingston <clivingston45@gmail.com>
2016-05-26 22:09:30 -0400

    matches index now working

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,9 +6,9 @@ class User < ActiveRecord::Base
 
   has_many :matches, dependent: :destroy
   has_many :active_relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
-  has_many :followers, through: :active_relationships, source: :follower
+  has_many :followers, through: :passive_relationships, source: :follower
   
 
   # here is the current email validation
